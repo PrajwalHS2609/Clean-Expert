decda9b6393b611ee78655b4da03bb3dc2a32e47
Prajwal H S <prajwalhs4india@gmail.com>
2025-12-19 21:23:32 +0530

    lp added

--- src/components/LandingPage/PaintLandingPage/LpPaintTestimonial/LpPaintTestimonial.jsx
+++ src/components/LandingPage/PaintLandingPage/LpPaintTestimonial/LpPaintTestimonial.jsx
@@ -3,7 +3,8 @@ import "./LpPaintTestimonial.css"
 const LpPaintTestimonial = () => {
   return (
     <div className='lpPaintTestimonial-container'>
-      
+      <div className=""></div>
+      <div className=""></div>
     </div>
   )
 }
