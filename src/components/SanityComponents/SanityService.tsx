d073abe1224f2d0f5fe702baeac75e4b817bcf87
Prajwal H S <prajwalhs4india@gmail.com>
2026-01-09 21:51:20 +0530

    sanity changes12

--- src/components/SanityComponents/SanityService.tsx
+++ src/components/SanityComponents/SanityService.tsx
@@ -103,15 +103,12 @@ export default function SanityServiceContent({
             {services.map((service, index) => (
               <div className="serviceHeader-servicesItem" key={index}>
                 <Link href={service.link || "/"}>
-                  {service.image && (() => {
-                    const imgUrl = urlFor(service.image);
-                    return imgUrl ? (
-                      <img
-                        src={imgUrl.url()}
-                        alt={service.title}
-                      />
-                    ) : null;
-                  })()}
+                  {service.image && (
+                    <img
+                      src={urlFor(service.image)?.url()}
+                      alt={service.title}
+                    />
+                  )}
                   <div className="serviceHeader-servicesItemCover">
                     <p>{service.title}</p>
                   </div>
