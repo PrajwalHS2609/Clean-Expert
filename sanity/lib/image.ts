3da91153b735b213779af5700088eb3c60fcacac
Prajwal H S <prajwalhs4india@gmail.com>
2026-01-09 22:11:51 +0530

    sanity changes13

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -2,14 +2,11 @@ import imageUrlBuilder from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { projectId, dataset } from "../env";
 
-const builder =
-  projectId && dataset
-    ? imageUrlBuilder({ projectId, dataset })
-    : null;
+const builder = imageUrlBuilder({
+  projectId,
+  dataset,
+});
 
 export function urlFor(source?: SanityImageSource) {
-  if (!builder || !source) {
-    return null;
-  }
-  return builder.image(source);
+  return source ? builder.image(source) : null;
 }
